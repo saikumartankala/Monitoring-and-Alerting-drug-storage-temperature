ab26fe11c66ebbe19ee1010cbee1a45a5483a96d
saikumartankala <66314427+saikumartankala@users.noreply.github.com>
2022-11-04 18:56:17 +0530

    Update conf.py

--- app/conf.py
+++ app/conf.py
@@ -1,1 +1,8 @@
-
+bolt_api_key = ""
+device_id = ""
+telegram_chat_id = ""
+telegram_bot_id = ""
+high_threshold_value = int(144/10.24)
+low_threshold_value = int(57/10.24)
+frame_size = 10
+mul_factor = 2
